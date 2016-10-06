36f2a70c53d3fd39ddba69d799cc549651db903e
Seth Voltz <seth@designgods.net>
2016-10-05 20:08:40 -0700

    Split classes to be more semantic, no more linter warnings

--- src/weatherd.rb
+++ src/weatherd.rb
@@ -28,10 +28,11 @@ class WeatherCube
 
   def start
     EventMachine.run do
-      puts 'Main event loop'
+      puts 'Starting main event loop...'
       @weather_timer = EventMachine::PeriodicTimer.new(15 * 60) do
         show_weather
       end
+      puts 'Started'
     end
   end
 
@@ -40,72 +41,27 @@ class WeatherCube
   end
 
   def show_weather
-    colors = weather_to_colors(
+    send_colors WeatherColors.new(
       Time.at(forecast.currently.time),
       forecast.currently.apparentTemperature,
       forecast.currently.icon
-    )
-    send_colors(colors)
-  end
-
-  def weather_to_colors(time, temperature, summary)
-    # Turn current time, temperature and description into a set of 6 colors
-    # Color order on cube
-    # 0 \   / 2
-    #  1 \ / 3
-    #   5 |
-    #   4 |
-    colors = [base_color(summary)] * 6
-
-    tip = tip_color(temperature)
-    if tip
-      colors[1] = hex_blend(colors[1], tip, 95)
-      colors[3] = hex_blend(colors[3], tip, 95)
-      colors[5] = hex_blend(colors[5], tip, 95)
-    end
-    colors
-  end
-
-  def base_color(summary)
-    # Summary can be one of:
-    #   clear-day, clear-night, rain, snow, sleet, wind, fog,
-    #   cloudy, partly-cloudy-day, or partly-cloudy-night
-    { 'clear-day' => '3ec3f5', 'clear-night' => '0d3779',
-      'rain' => '246dea',
-      'snow' => '88b5e3',
-      'sleet' => '65b9c2',
-      'wind' => 'aac399',
-      'fog' => '8ac5ae', 'cloudy' => '8ac5ae',
-      'partly-cloudy-day' => '9cc0c2', 'partly-cloudy-night' => '7996a7'
-    }[summary] || 'cccccc'
-  end
-
-  def tip_color(temperature)
-    case temperature
-    when NegativeInfinity..32 then '1200da'  # cold blue
-    when 60..75 then '27d06c'                # pleasant green
-    when 75..85 then 'e1ad1a'                # yellowy hot
-    when 85..95 then 'db5915'                # orangy hot
-    when 95..PositiveInfinity then 'd52a23'  # hot red
-    end
-  end
-
-  def hex_blend(mask, base, opacity)
-    Color::RGB.by_hex(base).mix_with(Color::RGB.by_hex(mask), opacity).hex
+    ).weather_to_colors
   end
 
   def send_colors(colors)
     # Send colors to edged
-    puts "colors received: #{colors.to_json}"
     socket = TCPSocket.open(CUBE_HOST, CUBE_PORT)
+
     socket.print({
       'command' => 'setColors',
       'colors' => colors,
       'mode' => 'ambient'
     }.to_json + "\r\n")
+
     if JSON.parse(socket.readline)['success'] == false
       # TODO: Handler errors
     end
+
     socket.print("\r\n")
     socket.close
   end
@@ -153,6 +109,65 @@ class WeatherCube
       'latitude' => ENV['LATITUDE'].to_f,
       'longitude' => ENV['LONGITUDE'].to_f
     }
+  end
+end
+
+# Handle color manipulation for colors
+class WeatherColors
+  def initialize(time, temperature, summary)
+    @time = time
+    @temperature = temperature
+    @summary = summary
+  end
+
+  # Turn current time, temperature and description into a set of 6 colors
+  # Color order on cube
+  # 0 \   / 2
+  #  1 \ / 3
+  #   5 |
+  #   4 |
+  def weather_to_colors
+    colors = [base_color(@summary)] * 6
+
+    tip = tip_color(@temperature)
+    if tip
+      colors[1] = hex_blend(colors[1], tip, 100) # Doing a full capture for now
+      colors[3] = hex_blend(colors[3], tip, 100) # due to daytime wash-out of
+      colors[5] = hex_blend(colors[5], tip, 100) # LED colors displayed.
+    end
+
+    colors
+  end
+
+  private
+
+  # Summary can be one of:
+  #   clear-day, clear-night, rain, snow, sleet, wind, fog,
+  #   cloudy, partly-cloudy-day, or partly-cloudy-night
+  def base_color(summary)
+    { 'clear-day' => '3ec3f5', 'clear-night' => '0d3779',
+      'rain' => '246dea',
+      'snow' => '88b5e3',
+      'sleet' => '65b9c2',
+      'wind' => 'aac399',
+      'fog' => '8ac5ae',
+      'cloudy' => '8ac5ae',
+      'partly-cloudy-day' => '9cc0c2', 'partly-cloudy-night' => '7996a7'
+    }[summary] || 'cccccc'
+  end
+
+  def tip_color(temperature)
+    case temperature
+    when NegativeInfinity..32 then '1200da'  # cold blue
+    when 60..75 then '27d06c'                # pleasant green
+    when 75..85 then 'e1ad1a'                # yellowy hot
+    when 85..95 then 'db5915'                # orangy hot
+    when 95..PositiveInfinity then 'd52a23'  # hot red
+    end
+  end
+
+  def hex_blend(mask, base, opacity)
+    Color::RGB.by_hex(base).mix_with(Color::RGB.by_hex(mask), opacity).hex
   end
 end
 
